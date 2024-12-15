63b4452729723aae0187a067624d56dc064cab13
Thollarkings <97956738+Thollarkings@users.noreply.github.com>
2024-12-15 09:18:36 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker.register('service-worker.js').then((registration) => {
+ if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('/service-worker.js').then((registration) => {
             console.log('ServiceWorker registration successful with scope: ', registration.scope);
         }).catch((error) => {
             console.log('ServiceWorker registration failed: ', error);
         });
-    });
-}
+    }
 
 document.addEventListener('DOMContentLoaded', () => {
     const form = document.getElementById('form');
