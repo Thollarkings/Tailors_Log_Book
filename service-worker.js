24734ac76ce3e990f3000a30f5750e0feadba37e
Thollarkings <97956738+Thollarkings@users.noreply.github.com>
2024-12-15 09:44:19 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,11 @@
 self.addEventListener('install', (event) => {
-    console.log('Service Worker: Installing...');
     event.waitUntil(
         caches.open('v1').then((cache) => {
-            console.log('Service Worker: Caching files...');
             return cache.addAll([
                 '/',
                 '/index.html',
                 '/styles.css',
                 '/script.js',
-                '/manifest.json',
                 '/icon-192x192.png',
                 '/icon-512x512.png'
             ]);
@@ -17,7 +14,6 @@ self.addEventListener('install', (event) => {
 });
 
 self.addEventListener('fetch', (event) => {
-    console.log('Service Worker: Fetching...', event.request);
     event.respondWith(
         caches.match(event.request).then((response) => {
             return response || fetch(event.request);
